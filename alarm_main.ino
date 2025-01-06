512c3b0d9b831ec5d69eb70b6e018e8b02e0f8ed
A6 <a6eline@gmail.com>
2025-01-06 16:41:34 +0000

    first commit to upload all files to git

---
+++ alarm_main.ino
@@ -0,0 +1,49 @@
+
+// This is a initial test of using the DS3231 and LiquidCrystal Libraries
+// I will be trying to display current time on the LCD screen
+
+#include "RTC_and_LCD.h"
+#include "buzzer.h"
+
+// SQW PIN can not interfere with LCD pins or the screen will print unusual things...
+#define CLOCK_INTERRUPT_PIN 8
+#define BUTTON_PIN 10
+
+void setup() {
+  checkRTC();
+
+  pinMode(BUTTON_PIN, INPUT); // setting button to be an input pin 
+  pinMode(CLOCK_INTERRUPT_PIN, INPUT_PULLUP); // Setting sqw pinmode to high  
+  attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), play_song, FALLING); // The interupt is triggered when the pin falls from high to low
+  // attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), play_song, FALLING); --> this will become interupt for buzzer soon
+
+  Serial.begin(9600); // Setup for serial monitor 
+  Serial.println("Alarm Clock with DS3231 module and RTClib library..."); // Introduction
+
+  timer_second(3);
+  count_second(3);
+  // play_song();
+}
+
+void loop() {
+  display_time();
+
+  if(alarm_status() == 1) { // while statement doesnt work
+    play_song();
+
+    if(digitalRead(BUTTON_PIN == HIGH)) { 
+      clear();
+    }
+  }
+  else {
+    silence();
+  }
+}
+
+void count_second(int n) {
+
+  for (int i = 1; i <= n; i++) { // Loop 10 times
+    Serial.println(i); // Print the current count value [2, 3, 5]
+    delay(1000); // Add a small delay between prints [3, 9]
+  }
+}
