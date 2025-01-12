822b94de0ce243a5088eafdf5f8e6db1c9d6b07c
adeline <a6eline@gmail.com>
2025-01-12 16:27:59 +0000

    made a new 'reset_alarm' function to fix firing bug

--- RTC_and_LCD.h
+++ RTC_and_LCD.h
@@ -10,7 +10,8 @@ void display_time(void);
 void alarm_setup(void);
 void timer_second(int n);
 void set_alarm(void);
-bool alarm_status(void); // checks if the alarm has been fired and returns true or false
-void clear(void);
+// bool alarm_status(void); // checks if the alarm has been fired and returns true or false
+void reset_alarm(int n);
+void stop_alarm(void);
 
 #endif
