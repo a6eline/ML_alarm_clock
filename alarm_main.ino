fa78de0b8e579027242c48fdb142c973202d64ac
adeline <a6eline@gmail.com>
2025-01-12 16:25:38 +0000

    updated function names for new reset_alarm func

--- alarm_main.ino
+++ alarm_main.ino
@@ -12,29 +12,33 @@
 
 void setup() {
   checkRTC();
+  alarm_setup();
 
   pinMode(BUTTON_PIN, INPUT); // setting button to be an input pin 
   pinMode(CLOCK_INTERRUPT_PIN, INPUT_PULLUP); // Setting sqw pinmode to high  
   attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), play_song, FALLING); // The interupt is triggered when the pin falls from high to low
-  // attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), play_song, FALLING); --> this will become interupt for buzzer soon
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), play_song, FALLING); //--> this will become interupt for buzzer soon
 
   Serial.begin(9600); // Setup for serial monitor 
   Serial.println("Alarm Clock with DS3231 module and RTClib library..."); // Introduction
 
   timer_second(3);
   count_second(3);
-  // play_song();
 }
 
 void loop() {
   display_time();
 
-  if(alarm_status() == 1) { // while statement doesnt work
+  if(digitalRead(CLOCK_INTERRUPT_PIN) == LOW) { 
     play_song();
-
-    if(digitalRead(BUTTON_PIN == HIGH)) { 
-      clear();
+    if(digitalRead(BUTTON_PIN) == HIGH) {       
+      Serial.println("Button Pressed");
+      silence();
+      reset_alarm(1);
     }
   }
+  // if(digitalRead(BUTTON_PIN) == HIGH) { 
+  //   Serial.println("Button Pressed");
+  // }
 }
 
