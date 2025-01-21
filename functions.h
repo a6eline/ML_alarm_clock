47ce3b37df828b62b83aec97a629574b0385aba2
adeline <a6eline@gmail.com>
2025-01-21 23:37:08 +0000

    added LED functions so I can quietly test my code when i am outside or if it is late

--- functions.h
+++ functions.h
@@ -4,15 +4,20 @@
 #define FUNCTIONS_H
 #include <Arduino.h>
 
-#define BUTTON_PIN 10
+#define LED_PIN 3
+#define BUTTON_PIN 2
+
+//------------------------SERIAL-------------------------------
+void serial_setup(void);
+
+//------------------------LED-------------------------------
+void led_setup(void);
+void led_status(int status); 
 
 //------------------------BUTTON-------------------------------
 void button_setup(void);
 bool button_status(void);
 void on_button();
-
-//------------------------BUTTON-------------------------------
-void serial_setup(void);
 
 //------------------------MISC-------------------------------
 void count_second(int n);
