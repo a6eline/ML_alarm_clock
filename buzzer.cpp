e178b8b16e6f46ad1a4f92d75ff5dbae565743b6
adeline <a6eline@gmail.com>
2025-01-24 05:40:17 +0000

    removed uneeded buzzer functions

--- buzzer.cpp
+++ buzzer.cpp
@@ -4,6 +4,7 @@
 
 void buzzer_setup(void) {
   pinMode(BUZZER_PIN, OUTPUT);
+  digitalWrite(BUZZER_PIN, LOW);
 }
 
 void silence(void) {
@@ -13,18 +14,5 @@ void silence(void) {
 
 void beep(void) {
   digitalWrite(BUZZER_PIN, HIGH);
-}
-
-// do re mi fa so
-void play_song(void) {
-  // for(int n, )
-  Serial.println(" Do Re Mi Fa So. . .");
-  tone(BUZZER_PIN, 440); delay(1000); // A4
-  tone(BUZZER_PIN, 494); delay(1000); // B4
-  tone(BUZZER_PIN, 523); delay(1000); // C4
-  tone(BUZZER_PIN, 587); delay(1000); // D4
-  tone(BUZZER_PIN, 659); delay(1000); // E4
-  tone(BUZZER_PIN, 698); delay(1000); // F4 
-  tone(BUZZER_PIN, 784); delay(1000); // G4
-  noTone(BUZZER_PIN); delay(200); // short pause
+  Serial.print("Buzzer on!!!! BEEEEEEEEEEEP. . .");
 }
