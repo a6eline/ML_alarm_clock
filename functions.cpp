13e667a436eb6aaa4a97c7ecfc802eb0f49c84f8
adeline <a6eline@gmail.com>
2025-01-26 19:11:28 +0000

    updated functions formats and comments

--- functions.cpp
+++ functions.cpp
@@ -1,43 +1,59 @@
-// this is the file for all the c++ functions needed in the arduino
-// this is so that the project participants who are not familiar with C++ or Arduino keywords can still participate 
+
+//----------------------------------------------------------------functions.cpp------------------------------------------------------------------------------------------------
+
+  // this is the file for all the c++ functions needed in the arduino
+  // this is so that the project participants who are not familiar with C++ or Arduino keywords can still participate 
 
 #include "functions.h"
-#include "buzzer.h"
 
 //-------------------------------------------------------------------SERIAL------------------------------------------------------------------------------------------------
 
 // functions.cpp --- sets up the serial monitor
 void serial_setup(void) {
   Serial.begin(9600); // Setup for serial monitor 
-  while (!Serial);
+  while (!Serial); }
+
+//-------------------------------------------------------------------BUZZER------------------------------------------------------------------------------------------------
+// functions.cpp --- setting up the buzzer
+void buzzer_setup(void) {
+  pinMode(BUZZER_PIN, OUTPUT);
+  digitalWrite(BUZZER_PIN, LOW); 
+}
+
+// functions.cpp --- shut the buzzer up
+void silence(void) {
+  digitalWrite(BUZZER_PIN, LOW); 
+}
+
+// functions.cpp --- buzzer beeping noise 
+void beep(void) {
+  digitalWrite(BUZZER_PIN, HIGH); 
 }
 
 //-------------------------------------------------------------------LED------------------------------------------------------------------------------------------------
 
 // functions.cpp --- void function, sets up the LED_PINs 
 void led_setup(void) {
-  pinMode(LED_PIN, OUTPUT);
-}
+  pinMode(LED_PIN, OUTPUT); 
+  }
 
 // functions.cpp --- a void function with parameters 1 to turn on LED, 0 to turn it off.
 void led_status(int status) {
   if (status == 1) {
-    digitalWrite(LED_PIN, HIGH);
-  }
+    digitalWrite(LED_PIN, HIGH); }
   else if (status == 0) {
-    digitalWrite(LED_PIN, LOW);
-  }
+    digitalWrite(LED_PIN, LOW); }
   else {
-    Serial.println("LED function input must be 0 or 1");
-  }
+    Serial.println("LED function input must be 0 or 1"); }
 }
 
 //-------------------------------------------------------------------BUTTON------------------------------------------------------------------------------------------------
 
 // functions.cpp --- sets up the button pins, attatches a silence interupt.
 void button_setup(void) {
-  pinMode(BUTTON_PIN, INPUT); // setting button to be an input pin 
+  pinMode(BUTTON_PIN, INPUT);  // setting button to be an input pin 
   // attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), on_button, FALLING); 
+  //    ^ uncommented because not needed with FSM, but maybe can include if you want
 }
 
 // functions.cpp --- checks if button has been pressed + debounce checker of 50ms
@@ -55,12 +71,14 @@ bool button_status(void) {
 }
 
 // functions.cpp --- if button is on, it will serial print ("Button pressed!")
+//    ^ this is for attachInterupt function in button_setup()
 void on_button() {
   Serial.println("Button Pressed!");
 }
 
 //-------------------------------------------------------------------MISC------------------------------------------------------------------------------------------------
 
+// functions.cpp --- counts 3 seconds without delay, doesnt work tho, i'll sort that out later
 void count_second(int n) {
   static unsigned long timer_millis = millis();
   const int one_sec = 1000;
@@ -68,9 +86,7 @@ void count_second(int n) {
   for (int i = 1; i <= n; i++) { // Loop n times
     if (millis() - timer_millis > one_sec ) {
       Serial.println(n);
-      timer_millis = millis();
-    }
-  }
+      timer_millis = millis(); } }
 }
 
 
