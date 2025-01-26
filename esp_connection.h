6bf76a1e378e8133a7c870bbde36784f0afa70f1
adeline <a6eline@gmail.com>
2025-01-26 19:11:07 +0000

    made an ESP connection .h file

---
+++ esp_connection.h
@@ -0,0 +1,13 @@
+
+//--------------------------------------------------------------esp_connection.h------------------------------------------------------------------------------------------------
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <Arduino.h>
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+// void read_MAC_address(void);
+
+#endif
