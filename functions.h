959b47797140db3a968853421937c61aba3b8b10
adeline <a6eline@gmail.com>
2025-01-26 19:11:30 +0000

    updated functions formats and comments

--- functions.h
+++ functions.h
@@ -1,25 +1,37 @@
-// this is the file for all the c++ functions needed in the arduino
+
+//----------------------------------------------------------------functions.h------------------------------------------------------------------------------------------------
 
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 #include <Arduino.h>
 
-#define LED_PIN 3
-#define BUTTON_PIN 2
+#define LED_PIN 1 // for when you have an LED
+#define BUTTON_PIN 2  
+#define BUZZER_PIN 3
 
-//------------------------SERIAL-------------------------------
-void serial_setup(void);
+//------------------------SERIAL----------------------------
+
+void serial_setup(void);  // serial monitor begin setup
 
 //------------------------LED-------------------------------
-void led_setup(void);
-void led_status(int status); 
 
-//------------------------BUTTON-------------------------------
-void button_setup(void);
-bool button_status(void);
-void on_button();
+void buzzer_setup(void);  // buzzer setup pins
+void silence(void);       // silences buzzer
+void beep(void);          // beeps the buzzer
 
-//------------------------MISC-------------------------------
-void count_second(int n);
+//------------------------LED-------------------------------
+
+void led_setup(void);     // sets up LEDs
+void led_status(int status); // toggles LED on or off
+
+//------------------------BUTTON----------------------------
+
+void button_setup(void);  // button setup
+bool button_status(void); // checks if button has been pressed
+void on_button();         // button pressed serial print message
+
+//------------------------MISC------------------------------
+
+void count_second(int n); // counts seconds to debug timers accuracy
 
 #endif
