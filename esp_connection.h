8b71e6479923054ad40ede2084c82675e54d81fa
adeline <a6eline@gmail.com>
2025-01-29 06:39:18 +0000

    fixed header guard bug, accidentally defined functions in this header file

--- esp_connection.h
+++ esp_connection.h
@@ -1,13 +1,11 @@
 
 //--------------------------------------------------------------esp_connection.h------------------------------------------------------------------------------------------------
 
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-#include <Arduino.h>
+#pragma once
 
+#include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
 
 // void read_MAC_address(void);
 
-#endif
